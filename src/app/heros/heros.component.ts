0596f18aaed0e2a9b2b1c06646bd45d5eb09e79b
githubanis <anissarker301@gmail.com>
2021-01-14 19:50:21 +0600

    msg

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
 
+  heros = HEROES;
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  ngOnInit(): void {
+
+  }
 }
